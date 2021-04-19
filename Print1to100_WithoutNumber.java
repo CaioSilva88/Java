c69aa471361df2e7479ce15443903fa8bb664aad
Caio Silva <61994442+CaioSilva88@users.noreply.github.com>
2021-04-19 16:55:43 -0300

    Create Print1to100_WithoutNumber.java

---
+++ Print1to100_WithoutNumber.java
@@ -0,0 +1,16 @@
+package pacoteOne;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		String s1 = ".";
+		String s2 = "..........";
+		for(int i=s1.length();i<= (s2.length()*s2.length());i++) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
